c5fa782307263f741a0f75a969a6ceeb6d550ea8
narutoben10af <limsieian@me.com>
2021-10-22 12:42:54 +0900

    Initial Commit

---
+++ Quotes App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes App
+//
+//  Created by Lim Si Eian on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
