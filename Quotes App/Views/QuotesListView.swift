292ccddb449435b25870e9001404940592ff4249
narutoben10af <limsieian@me.com>
2021-10-23 18:20:39 +0900

    Update to fit all device screen aspect ratio

--- Quotes App/Views/QuotesListView.swift
+++ Quotes App/Views/QuotesListView.swift
@@ -11,74 +11,81 @@ struct QuotesListView: View
 {
     @ObservedObject var model = QuotesModel()
     @State var searchText: String = ""
-
+    
     var searchResults: [Quote]
     {
         if searchText.isEmpty
         {
             return model.quotes
         }
-//        else if (model.quotes.contains(where: searchText))
-//        {
-//            return model.quotes.filter
-//            {
-//                $0.author.contains(searchText)
-//            }
-//        }
+        //        else if (model.quotes.contains(where: searchText))
+        //        {
+        //            return model.quotes.filter
+        //            {
+        //                $0.author.contains(searchText)
+        //            }
+        //        }
         else
         {
             return model.quotes.filter
             { item in
                 item.strQuote.localizedCaseInsensitiveContains(searchText) || item.author.localizedCaseInsensitiveContains(searchText)
             }
-//            return model.quotes.filter
-//            { [$0.strQuote, $0.author].contains(searchText)
-//            }
-
+            //            return model.quotes.filter
+            //            { [$0.strQuote, $0.author].contains(searchText)
+            //            }
+            
         }
     }
     var body: some View
     {
+        
         if #available(iOS 15.0, *) {
             NavigationView
             {
-                ScrollView
-                {
-                    VStack
+                GeometryReader
+                { geo in
+                    
+                    ScrollView
                     {
-//                        ForEach(model.quotes)
-                        ForEach(searchResults)
-                        { quote in
-                            NavigationLink(
-                                destination: QuotesDetailView(quote: quote),
-                                label:
-                                    {
-                                        ZStack
+                        VStack
+                        {
+                            //                        ForEach(model.quotes)
+                            ForEach(searchResults)
+                            { quote in
+                                NavigationLink(
+                                    destination: QuotesDetailView(quote: quote),
+                                    label:
                                         {
-                                            Image(quote.image).resizable().aspectRatio(contentMode: .fill).frame(minWidth: 300, idealWidth: 500, maxWidth: 600, minHeight: nil, idealHeight: 200, maxHeight: 200, alignment: .center).cornerRadius(20).clipped().padding(.vertical).shadow(radius: 10)
-                                            VStack
+                                            ZStack
                                             {
+                                                Image(quote.image).resizable().aspectRatio(contentMode: .fill).frame(width: geo.size.width, height: geo.size.height/2, alignment: .center).cornerRadius(20).clipped().padding(.vertical).shadow(radius: 10)
                                                 
-                                                Text(quote.strQuote).font(.largeTitle).fontWeight(.bold)
+                                                VStack
+                                                {
+                                                    
+                                                    Text(quote.strQuote).font(.largeTitle).fontWeight(.bold)
+                                                    
+                                                    Text(quote.author).font(.headline).fontWeight(.semibold)
+                                                    
+                                                }.padding().background(Color.gray.opacity(0.5)).cornerRadius(20).foregroundColor(.white)
                                                 
-                                                Text(quote.author).font(.headline).fontWeight(.semibold)
-                                                
-                                            }.padding().background(Color.gray.opacity(0.5)).cornerRadius(20).foregroundColor(.white)
+                                            }
                                             
-                                        }
-                                        
-                                    })
-                        }
-                    }.navigationTitle("Quotes")
+                                        })
+                            }
+                        }.navigationTitle("Quotes")
+                    }
+                    
                 }
             }.searchable(text: $searchText, prompt: "Search Quotes", suggestions: {
                 ForEach(model.quotes)
                 {suggestion in
                     Text(suggestion.strQuote).fontWeight(.bold).foregroundColor(Color.accentColor).searchCompletion(suggestion.strQuote).padding().background(Color.clear).opacity(2.0)
                     
-//                        .blur(radius: 0.5, opaque: false)
+                    //                        .blur(radius: 0.5, opaque: false)
                 }
-//                {Text("Unrelenting Guts").searchCompletion("Unrelenting Guts")}{Text("Confronting Courage").searchCompletion("Confronting Courage")}
+                //                {Text("Unrelenting Guts").searchCompletion("Unrelenting Guts")}{Text("Confronting Courage").searchCompletion("Confronting Courage")}
             }
             )
         } else {
@@ -87,14 +94,14 @@ struct QuotesListView: View
         
     }
     
-//        else
-//        {
-//            return model.quotes.filter
-//            {
-//                $0.author.contains(searchText)
-//            }
-//        }
-
+    //        else
+    //        {
+    //            return model.quotes.filter
+    //            {
+    //                $0.author.contains(searchText)
+    //            }
+    //        }
+    
 }
 
 struct ContentView_Previews: PreviewProvider
